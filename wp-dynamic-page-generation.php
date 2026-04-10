13d8c617b48857ef22eaeaece5756cd4f061cdaf
data4mat-group <dev@data4mat.se>
2026-04-10 13:08:30 +0200

    Changed contact to Author URI

--- wp-dynamic-page-generation.php
+++ wp-dynamic-page-generation.php
@@ -4,7 +4,7 @@
  * Description: Data-driven content engine with dynamic templates and blocks.
  * Version: 1.0.0
  * Author: Data4Mat AB
- * Email: dev@data4mat.se
+ * Author URI: https://data4mat.se
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
