25cea8229a417f1bca00fc361c34f32de79f5ac0
data4mat-group <dev@data4mat.se>
2026-04-10 12:55:49 +0200

    Added som conetent to the main phph file

---
+++ wp-dynamic-page-generation.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP Dynamic Page Generation
+ * Description: Data-driven content engine with dynamic templates and blocks.
+ * Version: 1.0.0
+ * Author: Data4Mat AB
+ * Email: dev@data4mat.se
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
